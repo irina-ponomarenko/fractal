32904b92993332583f0edfe7c48eb2847ca0447f
irinaponomarenko <ponomarenko02041993@gmail.com>
2021-06-02 16:05:23 +0300

    next page

--- js/main.js
+++ js/main.js
@@ -39,4 +39,25 @@ $(document).ready(function(){
        $(this).toggleClass('turn-btn');
     });
 
+    //-------------tabs---------------------//
+
+    const tabLinks = document.querySelectorAll(".tabs a");
+    const tabPanels = document.querySelectorAll(".tabs-panel");
+
+    for(let el of tabLinks) {
+        el.addEventListener("click", e => {
+            e.preventDefault();
+
+            document.querySelector('.tabs-item.active').classList.remove("active");
+            document.querySelector('.tabs-panel.active').classList.remove("active");
+
+            const parentListItem = el.parentElement;
+            parentListItem.classList.add("active");
+            const index = [...parentListItem.parentElement.children].indexOf(parentListItem);
+
+            const panel = [...tabPanels].filter(el => el.getAttribute("data-index") == index);
+            panel[0].classList.add("active");
+        });
+    }
+
 });
